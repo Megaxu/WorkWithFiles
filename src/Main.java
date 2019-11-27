5e0335fb060973298781a05402562f1cf31389e2
Mike <mixanik009@gmail.com>
2019-11-27 04:14:02 +0300

    Second Step

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.io.File;
+import java.io.FileInputStream;
 
 public class Main {
 
   public static void main(String[] args) {
+    firstStep();     // Обычное чтение файлов
+    secondStep();    // Через  inputStream
+  }
 
+  public static void firstStep() {
     String path = "H:\\Projects\\FilesForProjects".replaceAll("\\\\", "/");
     // Читал, что если такие слэши, то убирается кроссплатформенность, поэтому заменил их
     File folder = new File(path);
-    if(folder.isDirectory()) {
+    if (folder.isDirectory()) {
       File[] files = folder.listFiles();
 
-      for(File file : files) {
+      for (File file : files) {
         System.out.println(file);
       }
 
@@ -18,11 +23,28 @@ public class Main {
       newFolder.mkdir();
 
       newFolder.delete();
-    }
-    else {
+    } else {
       System.out.println("This is not a folder");
     }
+  }
 
+  public static void secondStep() {
+    StringBuilder builder = new StringBuilder();
 
+    try {
+      FileInputStream is = new FileInputStream("data/info.txt");
+      for (; ; ) {
+        int code = is.read();
+        if (code < 0) {
+          break;
+        }
+        char ch = (char)code;
+        builder.append(ch);
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    System.out.println(builder.toString());
   }
 }
